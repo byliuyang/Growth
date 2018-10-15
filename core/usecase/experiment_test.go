04d7011cc5c56878c06fdc960ada062d5587d2b9
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2018-10-15 14:18:33 -0700

    use sub test

--- core/usecase/experiment_test.go
+++ core/usecase/experiment_test.go
@@ -11,11 +11,13 @@ import (
 )
 
 func TestCreateExperiment(t *testing.T) {
-	rand.Seed(time.Now().Unix())
-	for i := 0; i < 10; i++ {
-		id := entity.ID(rand.Int63())
-		exp, err := CreateExperiment(id)
-		require.NoError(t, err)
-		require.Equal(t, id, exp.Owner)
-	}
+	t.Run("the owner of an experiment is correctly set", func(t *testing.T) {
+		rand.Seed(time.Now().Unix())
+		for i := 0; i < 10; i++ {
+			id := entity.ID(rand.Int63())
+			exp, err := CreateExperiment(id)
+			require.NoError(t, err)
+			require.Equal(t, id, exp.Owner)
+		}
+	})
 }
