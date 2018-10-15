24c556f139cce952ecc4fdfac11511c4928597b9
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2018-10-15 13:39:42 -0700

    no idea why it doesn't work

--- gql/e2e/test.ts
+++ gql/e2e/test.ts
@@ -1,31 +1,30 @@
-var ApolloClient = require('apollo-boost');
-var gql = require('graphql-tag');
+const assert = require('assert');
+const axios = require('axios');
 
-const client = new ApolloClient.ApolloClient({
-    uri: 'localhost:8080/graphql'
-});
-
-var assert = require('assert');
-describe('GraphQL', function() {
-    describe('event', function() {
-        it('push event should succeed', function() {
-
-            client.query({
-                query: gql`
-                query {
+describe('GraphQL', function () {
+    describe('event', function () {
+        it('push event should succeed', async function () {
+            let resp = await axios.post("http://localhost:8080/graphql", {
+                query: `query {
                     event(id: 1) {
                         id
                     }
+                }`
+            });
+            console.log(resp.data);
+            assert.equal(resp.data, {
+                errors: [
+                    {
+                        message: "event:1 not found",
+                        path: [
+                            "event"
+                        ]
+                    }
+                ],
+                data: {
+                    event: null
                 }
-                `,
             })
-                .then(data => console.log(data))
-                .catch(error => console.error(error));
-
-        });
-
-        it('push event should succeed', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
+        })
     });
 });
